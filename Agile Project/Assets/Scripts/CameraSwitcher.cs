3c31b6c93a9c5ae741b3c7902d5d8dc3598feb56
jordanbrown0606 <157506730+jordanbrown0606@users.noreply.github.com>
2024-03-21 17:19:51 -0500

    Added Cinemachine camera switcher and tank controls.

---
+++ Agile Project/Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,26 @@
+
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitcher : MonoBehaviour
+{
+    public Transform player;
+    public CinemachineVirtualCamera activeCamera;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            activeCamera.Priority = 1;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            activeCamera.Priority = 0;
+        }
+    }
+}
